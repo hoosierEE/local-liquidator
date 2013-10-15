6925d8b5fb33ea209e4aa268abf8498a42fda5e0
Alex Shroyer <ashroyer@gmail.com>
2013-10-15 13:30:30 -0400

    Create getGeo.js

---
+++ js/getGeo.js
@@ -0,0 +1,2 @@
+//This file should contain a javascript function which returns the browser's geolocation coordinates.
+//If it encounters an error it should return an error string or error code of some sort.
