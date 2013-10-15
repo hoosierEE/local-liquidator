50a867423bd8f43c1d989d5127e13643041ff3bc
Alex Shroyer <ashroyer@gmail.com>
2013-10-15 13:32:14 -0400

    Create drawMap.js

---
+++ js/drawMap.js
@@ -0,0 +1,6 @@
+//This file should accept as parameters:
+// a pair of coordinates for a center position
+// a list of markers which contain positions and text descriptions
+
+//Based on these inputs this function should return a map using the Google Maps API which
+// is centered on the "center position" and also displays the markers.
