de379101050e2a851c1492306c70911e5bdb1427
Alex Shroyer <ashroyer@gmail.com>
2013-10-16 16:30:39 -0400

    Update sketch.elm
    Put text fields into layout

--- sketch.elm
+++ sketch.elm
@@ -1,34 +1,40 @@
 import Window
 import Graphics.Input as Input
 
-{-
-visual elements of our app:
- title/tagline (always at top)
- listBar [addBox, list]
- map
- otherStuff
+(field,typedText) = Input.field "type here"
 
-When in landscape mode we want to see the listBar on the left and small,
- and [map, otherStuff] on the right and larger.
-In portrait mode we want to see the map full-width,
- but some area at the bottom to see the listBar's top,
- then the rest of the listBar,
- and finally the otherStuff
--}
 
-content (w,h) = let
+
+content (w,h) f tx = let
   w' = (\n m-> round <| (toFloat n) * m) w 0.75 -- 75% screen width
   w'' = w - w'  -- 25% screen width
   h' = round <| (toFloat h) * 0.75
-  ratio a b name = asText <| name ++ " width:height = " ++ show (toFloat (a) / toFloat(b))
-  mapBox wid hei = color green <| container wid hei middle <| ratio wid hei "map" --rectangle
+  
+  results = 
+    let parseString x = case x of
+        [] -> show "Your Ad Content"
+        _ -> show x
+    in parseString tx
+    
+  mapBox wid hei = color green <| container wid hei middle <| plainText "map"
   listBar wid = flow down [
-    color teal <| container wid 100 middle <| plainText "add item",
-    color white <| container wid h midTop <| plainText "list items"] --listAdd, list
-  otherStuff wid = color gray <| container wid h' middle <| plainText "other stuff"
+    color blue <| container wid 50 middle <| plainText "add item area"
+    , f
+    , plainText results
+    , color white <| container wid h midTop <| plainText "list items area"]
+  otherStuff wid = color gray <| container wid h' middle <| plainText "other stuff goes here"
+  
+  -- different visual modes:
   landscape = flow right [listBar w'', flow down [mapBox w' h, otherStuff w']]
   portrait = flow down [mapBox w h', flow right [listBar w'', otherStuff w']]
- in 
-  if w' > h then landscape else portrait
+  -- everything:
+  header = color red <| width w <| [markdown|
+<h1 style="font-family:Times, serif;text-align:center;">Local Liquidator</style>
+<h3 style="text-align:center;">Tagline</style>
+|]
+  rest = if w' > h then landscape else portrait
 
-main = content <~ Window.dimensions
+ in 
+  flow down [header, rest]
+
+main = content <~ Window.dimensions ~ field ~ typedText
