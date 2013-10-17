86ab635daaff8ad283300ca157834208dc062f33
Alex Shroyer <ashroyer@gmail.com>
2013-10-17 07:27:14 -0400

    Added more comments

--- js/drawMap.js
+++ js/drawMap.js
@@ -4,3 +4,8 @@
 
 //Based on these inputs this function should return a map using the Google Maps API which
 // is centered on the "center position" and also displays the markers.
+
+//Other potential parameters:
+// a rectangle of lat/lng bounds for setting the bounds of our map
+// other info such as <boolean> buySellStatus which could be used to make buyer markers 
+//  visually different from seller markers
