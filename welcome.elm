0b97906a39c9143a85b93a0752cdc029494f97fb
Alex Shroyer <ashroyer@gmail.com>
2013-10-20 22:37:58 -0400

    Update welcome.elm

--- welcome.elm
+++ welcome.elm
@@ -1,119 +1,74 @@
-<!DOCTYPE HTML>
+import Window
+import Graphics.Input as Input
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>
-            welcome
-        </title>
-        <script type="text/javascript" src="elm-runtime.js">
-            
-        </script>
-        <script type="text/javascript">
-            
-Elm.Main = function(elm){
-  var N = Elm.Native, _N = N.Utils(elm), _L = N.List(elm), _E = N.Error(elm), _J = N.JavaScript(elm), _str = _J.toString, $moduleName = 'Main';
-  var Text = Elm.Text(elm);
-  var Text = Elm.Text(elm);
-  var Basics = Elm.Basics(elm);
-  var Signal = Elm.Signal(elm);
-  var List = Elm.List(elm);
-  var Maybe = Elm.Maybe(elm);
-  var Time = Elm.Time(elm);
-  var Prelude = Elm.Prelude(elm);
-  var Graphics = Graphics || {};
-  Graphics.Element = Elm.Graphics.Element(elm);
-  var Color = Elm.Color(elm);
-  var Graphics = Graphics || {};
-  Graphics.Collage = Elm.Graphics.Collage(elm);
-  var Window = Elm.Window(elm);
-  var Graphics = Graphics || {};
-  Graphics.Input = Elm.Graphics.Input(elm);
-  var _op = {};
-  var _0 = Graphics.Input.field(_str('type here'));
-  var field = function(){
-    switch (_0.ctor) {
-      case '_Tuple2':
-        return _0._0;
-    }_E.Case($moduleName,'on line 4, column 21 to 44')}();
-  var typedText = function(){
-    switch (_0.ctor) {
-      case '_Tuple2':
-        return _0._1;
-    }_E.Case($moduleName,'on line 4, column 21 to 44')}();
-  var _7 = Graphics.Input.button(_str('Sell'));
-  var btnB = function(){
-    switch (_7.ctor) {
-      case '_Tuple2':
-        return _7._1;
-    }_E.Case($moduleName,'on line 6, column 18 to 37')}();
-  var buttonB = function(){
-    switch (_7.ctor) {
-      case '_Tuple2':
-        return _7._0;
-    }_E.Case($moduleName,'on line 6, column 18 to 37')}();
-  var _14 = Graphics.Input.button(_str('Buy'));
-  var btnA = function(){
-    switch (_14.ctor) {
-      case '_Tuple2':
-        return _14._1;
-    }_E.Case($moduleName,'on line 5, column 18 to 36')}();
-  var buttonA = function(){
-    switch (_14.ctor) {
-      case '_Tuple2':
-        return _14._0;
-    }_E.Case($moduleName,'on line 5, column 18 to 36')}();
-  var btnz = A2(Graphics.Element.flow, Graphics.Element.right, _J.toList([buttonA,buttonB]));
-  var content = F3(function(arg3, textbox, tx){
-    return function(){
-      switch (arg3.ctor) {
-        case '_Tuple2':
-          return function(){
-            var w$ = List.maximum(A2(List.map, Graphics.Element.widthOf, _J.toList([btnz,textbox])));
-            var results = function(){
-              var parseString = function(x){
-                return function(){
-                  switch (x.ctor) {
-                    case '[]':
-                      return Text.plainText(_str('Your Ad Content'));
-                  }
-                  return Text.plainText(x);}();};
-              return parseString(tx);}();
-            var md2 = Text.text('<div style="height:0;width:0;">&nbsp;</div><h3 id="what">What</h3>\n<p>Local Liquidator is a peer-to-peer textbook resale app with a focus on <em>truly</em> local commerce.</p>\n<h3 id="how">How</h3>\n<p>Post an ad, your ad will appear to other students using the app nearby. Optional CAS login for IUPUI students.</p><div style="height:0;width:0;">&nbsp;</div>');
-            var otherStuff = Graphics.Element.width((arg3._0-w$))(md2);
-            var md1 = Text.text('<div style="height:0;width:0;">&nbsp;</div><iframe width=400 height=300 src="http://www.local-liquidator.com/map.html">\n  <p>\nYour browser does not support iframes.\n</p>\n</iframe>\n\n\n<div style="height:0;width:0;">&nbsp;</div>');
-            var mapBox = F2(function(wid, hei){
-              return Graphics.Element.color(Color.green)(A3(Graphics.Element.container, wid, hei, Graphics.Element.middle)(md1));});
-            var listBar = function(wid){
-              return A2(Graphics.Element.flow, Graphics.Element.down, _J.toList([Graphics.Element.color(Color.blue)(A3(Graphics.Element.container, wid, 30, Graphics.Element.middle)(Text.plainText(_str('add item area')))),textbox,btnz,results,Text.plainText(_str('list items area'))]));};
-            var landscape = A2(Graphics.Element.flow, Graphics.Element.right, _J.toList([listBar(w$),A2(Graphics.Element.flow, Graphics.Element.down, _J.toList([A2(mapBox, (arg3._0-w$), arg3._1),otherStuff]))]));
-            var header = Graphics.Element.color(Color.red)(Graphics.Element.width(arg3._0)(Text.text('<div style="height:0;width:0;">&nbsp;</div>\n<h1 style="font-family:Times, serif;text-align:center;">\nLocal Liquidator\n</style>\n<h3 style="text-align:center;">\nTagline\n</style>\n\n<div style="height:0;width:0;">&nbsp;</div>')));
-            var h$ = Basics.round((Basics.toFloat(arg3._1)*0.75));
-            var portrait = A2(Graphics.Element.flow, Graphics.Element.down, _J.toList([A2(mapBox, arg3._0, h$),A2(Graphics.Element.flow, Graphics.Element.right, _J.toList([listBar(w$),otherStuff]))]));
-            var rest = ((_N.cmp((arg3._0-w$),arg3._1)>0) ? landscape : (Basics.otherwise ? portrait : _E.If($moduleName,'on line 64, column 10 to 54')));
-            var footer = Graphics.Element.color(Color.red)(Graphics.Element.width(arg3._0)(Text.text('<div style="height:0;width:0;">&nbsp;</div><em>\n<center>\nFooter goes here.\n</center>\n<p></em></p><div style="height:0;width:0;">&nbsp;</div>')));
-            return A2(Graphics.Element.flow, Graphics.Element.down, _J.toList([header,rest,footer]));}();
-      }_E.Case($moduleName,'between lines 10 and 71')}();});
-  var main = A2(Signal._op['~'], A2(Signal._op['~'], A2(Signal._op['<~'], content, Window.dimensions), field), typedText);
-  return elm.Main = {
-    _op : _op, 
-    field : field, 
-    typedText : typedText, 
-    btnA : btnA, 
-    buttonA : buttonA, 
-    btnB : btnB, 
-    buttonB : buttonB, 
-    btnz : btnz, 
-    content : content, 
-    main : main};};
-        </script>
-    </head>
-    <body>
-        <script type="text/javascript">
-            Elm.fullscreen(Elm.Main)
-        </script>
-        <noscript>
-            
-        </noscript>
-    </body>
-</html>
+(field,typedText) = Input.field "type here"
+(buttonA,btnA) = Input.button "Buy"
+(buttonB,btnB) = Input.button "Sell"
+
+btnz = flow right [buttonA, buttonB]
+
+content (w,h) textbox tx = let
+
+---- beginning of huge Let statement ----
+
+  w' = maximum <| map widthOf [btnz,textbox] -- width of sidebar
+  h' = round <| (toFloat h) * 0.75
+  
+  md1 = [markdown|
+  
+<iframe width=400 height=300 src="http://www.local-liquidator.com/map.html">
+  <p>Your browser does not support iframes.</p>
+</iframe>
+
+|]
+
+  md2 = [markdown|
+  
+### What
+
+Local Liquidator is a peer-to-peer textbook resale app with
+a focus on *truly* local commerce.
+
+### How
+
+Post an ad, your ad will appear to other students using the 
+app nearby.  Optional CAS login for IUPUI students.
+
+|]
+
+
+  results = 
+    let parseString x = case x of
+        [] -> plainText  "Your Ad Content"
+        _ -> plainText x
+    in parseString tx
+  
+  -- oure main 3 visual components --  
+  mapBox wid hei = color green <| container wid hei middle <| md1
+  listBar wid = flow down [
+    color blue <| container wid 30 middle <| plainText "add item area"
+    , textbox
+    , btnz
+    , results
+    , plainText "list items area"]
+  otherStuff = width (w - w') <| md2
+  
+  -- different visual modes:
+  landscape = flow right [listBar w', flow down [mapBox (w - w') h, otherStuff]]
+  portrait = flow down [mapBox w h', flow right [listBar w', otherStuff]]
+  -- everything:
+  header = color red <| width w <| [markdown|
+<h1 style="font-family:Times, serif;text-align:center;">Local Liquidator</style>
+<h3 style="text-align:center;">Tagline</style>
+|]
+  rest = if (w - w') > h then landscape else portrait
+  footer = color red <| width w <| [markdown|
+<em><center>Footer goes here.</center></em>
+  |]
+-- end of huge Let statement< ----
+
+ in 
+  flow down [header, rest, footer]
+
+-- puny main function which takes in all the signals
+main = content <~ Window.dimensions ~ field ~ typedText
