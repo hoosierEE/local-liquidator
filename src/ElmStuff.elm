545f4daebf406b3a139c81f6db209d230f51d182
hoosierEE <ashroyer@gmail.com>
2013-10-28 21:50:16 -0400

    added markers.html

--- src/ElmStuff.elm
+++ src/ElmStuff.elm
@@ -17,4 +17,17 @@ clickLocations =
   Nothing -> []
  in foldp update [] events
 
-main = color green <| container 280 280 middle <| asText "Hello from inside a <div>!"
+scene (w,h) locs =
+  let drawPentagon (x,y) =
+          ngon 5 20 |> filled (hsva (toFloat x) 1 1 0.7)
+                    |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+                    |> rotate (toFloat x)
+  in  layers [ collage w h (map drawPentagon locs)
+             , plainText "Click to stamp a pentagon." ]
+
+main = lift2 scene Window.dimensions clickLocations
+
+-- Export the number of stamps to JS with the name "count"
+stampCount = lift (JS.fromInt . length) clickLocations
+foreign export jsevent "count"
+    stampCount : Signal JS.JSNumber
