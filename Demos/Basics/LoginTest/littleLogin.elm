5e7166da58cd7ef7f257ee860dbebf486cc8d46d
hoosierEE <ashroyer@gmail.com>
2013-11-19 22:43:00 -0500

    another login test page

--- Demos/Basics/LoginTest/littleLogin.elm
+++ Demos/Basics/LoginTest/littleLogin.elm
@@ -1,12 +1,12 @@
 import Http
 
-getLogin req = Http.send <| lift (\r -> Http.post r "") req
-
 prettyPrint res = case res of
   Http.Waiting -> plainText ""
   Http.Failure _ _ -> plainText ""
   Http.Success a -> plainText a
 
-defaultValue = constant ""
+getLogin req = Http.send <| lift (\r -> Http.post r "") req
+
+defaultValue = constant "/php/getUser.php"
 
 main = prettyPrint <~ getLogin defaultValue
