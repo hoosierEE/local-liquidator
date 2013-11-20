052b2581bbfaa77cbcd7a021123506c8bc81f2fb
hoosierEE <ashroyer@gmail.com>
2013-11-20 16:55:24 -0500

    fixed? checkISBN preview

--- Demos/Basics/Utils/Isbn.elm
+++ Demos/Basics/Utils/Isbn.elm
@@ -3,10 +3,7 @@ import Http
 import Maybe
 
 scriptSrc : String
-scriptSrc = "/php/checkISBN.php"
-
-responses : Signal String -> Signal (Http.Response String)
-responses s = Http.sendGet (lift id s)
+scriptSrc = "/php/checkISBN.php?isbn="
 
 prettyPrint : Http.Response String -> String 
 prettyPrint res = case res of
@@ -14,17 +11,11 @@ prettyPrint res = case res of
   Http.Failure _ _ -> "404" -- for debugging
   Http.Success a   -> a 
 
-getLogin : Signal String -> Signal (Http.Response String)
-getLogin req = Http.send <| lift (\r -> Http.post r "") req 
-
--- HTTP control
-sendRequest : Signal String
-sendRequest = keepWhen (constant True) "" <| constant scriptSrc
-
-helloUser : Signal String
-helloUser = prettyPrint <~ getLogin sendRequest
+responses : Signal String -> Signal (Http.Response String)
+responses s = Http.sendGet (queryString <~ s)
 
 queryString : String -> String
 queryString str = scriptSrc ++ str
 
-isbnString str = prettyPrint <~ (getLogin (queryString <~ str))
+isbnString : Signal String -> Signal String
+isbnString str = prettyPrint <~ responses (dropRepeats str)
