9dd63d60bef9ed964464e5cda5fb087b001b32ee
hoosierEE <ashroyer@gmail.com>
2013-11-22 22:50:43 -0500

    included all params in storeAdTest.html Http.post request

--- Source/Utils/StoreAd.elm
+++ Source/Utils/StoreAd.elm
@@ -1,29 +1,28 @@
 import Http
 import Graphics.Input as Input
 
-(butn, press) = Input.button "Store Ad"
+-- data
+(butn, press) = Input.button "Store Ad" 
+url = "/php/storeAd.php?Title=&ISBN10=1234567890&ISBN13=123456789abc&imageURL=someImgUrl&ExpTime=later&Description=Short&Condition=new&Price=12.42&user=ashroyer&Adtype=seller&Lat=1&Lon=2" 
 
-url title isbn10 isbn13 = 
-  "/php/storeAd.php?Title=" ++ title ++ 
-  "&ISBN10=" ++ isbn10 ++
-  "&ISBN13=" ++ isbn13
-
+-- button control
 isClicked evt = let always value signal = lift (\_->value) signal
   in merge (always False (delay 0 evt)) (always True evt)
-
 sendable = isClicked press
 
-sendRequest = keepWhen sendable "0" <| lift3 url (constant "randomBookTitle") (constant  "1234123412") (constant "1234123412341")
-
+-- http control
+sendRequest = keepWhen sendable "0" <| constant url
 getLogin req = Http.send <| lift (\r -> Http.post r "") req
+loginResponse = lift prettyPrint (getLogin sendRequest)
 
+-- format the returned data 
 prettyPrint res = case res of
   Http.Waiting -> plainText "waiting"
   Http.Failure _ _ -> plainText "aw dang, 404"
   Http.Success a -> plainText a
 
-loginResponse = lift prettyPrint (getLogin sendRequest)
-note = plainText <| 
-  """ click to Http.post the url "/php/storeAd.php?Title=randomBookTitle&ISBN10=1234567890" """
+-- display
+note = width 100 . plainText <| "click to post the URL to " ++ url 
 elems = flow right [butn, note]
+
 main = flow down <~ combine [constant elems, loginResponse]
