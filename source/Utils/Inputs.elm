87d3ed4f244032d29c15139e497ff3bf6c3db765
hoosierEE <ashroyer@gmail.com>
2013-12-02 23:45:30 -0500

    better isbn validation

--- source/Utils/Inputs.elm
+++ source/Utils/Inputs.elm
@@ -6,8 +6,9 @@ import Utils.Validation (validate, btnClicked)
 import Utils.Header (helloUser)
 
 -- Logic
-validIsbn  = (\n->validate n True ) <~ isbnSignal
-validPrice = (\n->validate n False) <~ priceSignal
+validIsbn    = (\n->validate n "isbn"   ) <~ isbnSignal
+validPrice   = (\n->validate n "price"  ) <~ priceSignal
+validExpTime = (\n-> validate n "expire") <~ expTimeSignal
 
 isbnRequestString =
   let ajax s = case s of
@@ -19,29 +20,33 @@ adDisplay = plainText <~ constant "" -- (prettyPrint <~ adRequestString)
 
 adRequestString =
   let
-    baseUrl adType price user isbn = "/php/storeAd.php?user=" ++ user ++ "&ExpTime=10&Price=" ++ price
+    baseUrl adType price user isbn expTime =
+      "/php/storeAd.php?user=" ++ user
+      ++ "&ExpTime=" ++ expTime 
+      ++ "&Price=" ++ price
       ++ "&Adtype=" ++ adType
       ++ "&ISBN10=" ++ isbn
     whichButton =
       let ss = keepWhen (btnClicked sellEvent) "" (constant "sell")
           bs = keepWhen (btnClicked buyEvent) "" (constant "buy")
       in merge ss bs
-  in sendReq (sampleOn whichButton (baseUrl <~ whichButton ~ validPrice ~ helloUser ~ validIsbn)) "get"
+  in sendReq (sampleOn whichButton (baseUrl <~ whichButton ~ validPrice ~ helloUser ~ validIsbn ~ validExpTime)) "get"
 
 -- Model
-(isbn , isbnSignal ) = Input.field "ISBN"
-(price, priceSignal) = Input.field "Price"
-(buy  , buyEvent   ) = Input.button "Buy"
-(sell , sellEvent  ) = Input.button "Sell"
-
+(isbn  , isbnSignal   ) = Input.field "ISBN"
+(price , priceSignal  ) = Input.field "Price"
+(expire, expTimeSignal) = Input.field "Expire Time"
+(buy   , buyEvent     ) = Input.button "Buy"
+(sell  , sellEvent    ) = Input.button "Sell"
+                          
 -- View
 controls =
   let
-    arranged a b c d = 
-      let h = map heightOf [a,b,c,d] |> foldr (+) 0
-          w = map widthOf [a,b,c,d] |> maximum
+    arranged a b c d f = 
+      let h = map heightOf [a,b,c,d,f] |> foldr (+) 0
+          w = map widthOf [a,b,c,d,f] |> maximum
           border = 10
           title = width 200 . centered . Text.height 20 . Text.color darkYellow <| toText "Controls"
-      in flow down [title, flow down [a,b,c,d] |> container (w + border) (h + border) middle |> color lightBlue]
+      in flow down [title, flow down [a,b,c,d,f] |> container (w + border) (h + border) middle |> color lightBlue]
     btngroup = constant <| flow right [buy, sell]
-  in arranged <~ isbn ~ price ~ btngroup ~ adDisplay
+  in arranged <~ isbn ~ price ~ expire ~ btngroup ~ adDisplay
--- source/Utils/Validation.elm
+++ source/Utils/Validation.elm
@@ -2,15 +2,24 @@ module Utils.Validation (validate, btnClicked) where
 import String
 import Char
 
--- Input Valication Logic
-
-validate : String -> Bool -> String
-validate x isIsbn = case isIsbn of
-  -- hack "0" around the "show own html" issue
-  True -> if (String.all Char.isDigit x && (String.length x == 10 || String.length x == 13)) then x else ""
-  False -> case (String.toFloat x) of
-    Nothing -> ""
-    Just x -> show x
+validate : String -> String -> String
+validate str inputType = case inputType of
+  -- valid isbn10: 10 digits or 9 digits ++ X
+  -- valid isbn13: 13 digits
+  "isbn"   ->
+    let isbnDigits = String.filter Char.isDigit str
+    in case String.length isbnDigits of
+      9 -> isbnDigits ++ "X"
+      10 -> isbnDigits
+      13 -> isbnDigits
+      _ -> ""
+  "price"  -> case (String.toFloat str) of
+      Nothing -> ""
+      Just str  -> show str
+  "expire" -> let defaultTime = "60" in
+    case String.toInt str of
+      Nothing -> defaultTime -- default expires in 1 hour
+      Just str  -> show str
 
 btnClicked : Signal () -> Signal Bool
 btnClicked btnPress = let always value signal = lift (\_ -> value) signal
