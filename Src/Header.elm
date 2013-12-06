8457e240629735a8a176100a68d8fc63bc505a26
hoosierEE <ashroyer@gmail.com>
2013-12-06 15:28:58 -0500

    new header

---
+++ Src/Header.elm
@@ -0,0 +1,35 @@
+module Header where
+
+import Window
+import Utils.Layout as Layout
+import Utils.Rest as Rest
+
+title w msg = let
+    h = toFloat <| max 30 (w `div` 20)
+    hdr = Layout.headerGen w h darkOrange "local-liquidator" 
+    underLine = spacer w 2 |> color darkOrange
+    scene = layers [flow down [ hdr, navRow w, underLine ], msg ]
+  in scene
+
+welcomeMessage = let
+    txt = Layout.headerGen 100 18 darkOrange 
+    msg rsp = case rsp of
+      "" -> txt "~"
+      "Guest" -> txt "Hi Guest, log in to do stuff"
+      _  -> txt <| "Welcome " ++ rsp
+  in msg <~ Rest.helloUser
+
+navigation = [ ("/MakeAd.html"       , "Create Ad")
+             , ("/MyAds.html"        , "My Ads"   )
+             , ("/About.html"        , "About"    )
+             , ("/php/php_cas.php"   , "Login"    ) 
+             , ("/php/cas_logout.php", "Logout"   ) ]
+
+navButtons = let
+    navButton (href, name) = link href <| Layout.buttonGen name
+  in map navButton navigation
+
+navRow w = let bar = flow right navButtons 
+  in container w (heightOf bar) middle bar
+
+main = title <~ Window.width ~ welcomeMessage
--- Src/Utils/Layout.elm
+++ Src/Utils/Layout.elm
@@ -9,6 +9,7 @@ leftWidth       = makerWidth `div` 3 - borderWidth `div` 2
 rightWidth      = makerWidth - leftWidth - borderWidth
 borderWidth     = maximum [makerWidth `div` 20, 10]
 rowHeight       = 38
+inputColor      = rgba 255 199 0 0.2
 
 -- Elements
 padding         = spacer borderWidth borderWidth 
@@ -16,26 +17,23 @@ hr              = spacer makerWidth 2 |> color darkOrange
 heads str       = constant <| flow down [ headerGen makerWidth 28 darkOrange str, hr ] 
 
 -- Arrangement
-table a b       =
-  let
+table a b = let
     leftSide            = map (\n-> container leftWidth rowHeight midRight <| plainText n) a
     rightSide           = map (\n-> container rightWidth rowHeight midLeft <| width (rightWidth - 2) n) b
     row (title,content) = flow right [ title, padding, content ]
-  in color (rgba 255 199 0 0.2) <| flow down <| map row <| zip leftSide rightSide
+  in color inputColor  <| flow down <| map row <| zip leftSide rightSide
 
 -- All-purpose Header
 headerGen w h c = width w . centered . Text.height h . Text.color c . toText
 
 -- Custom Button Builder
 butn = customButtons ""
-buttonGen str =
-  let
-    box c1 c2 =
-      let
+buttonGen str = let
+    box c1 c2 = let
         word     = width halfWidth . centered . Text.color darkRed . Text.height 20 . toText <| str
         grad     = linear (0,29) (0,-29) [ (0,c1),(1,c2) ]
         emptyBtn = gradient grad <| rect (toFloat halfWidth - 2) 36
-      in collage halfWidth 38 [ emptyBtn, toForm word ]
+    in collage halfWidth 38 [ emptyBtn, toForm word ]
   in butn.customButton str (box white lightOrange)
                            (box white darkOrange )
                            (box red   lightOrange)
