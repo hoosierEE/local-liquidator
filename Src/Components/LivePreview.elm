d563b50b092b98dcdabe467837c1d72ccd90433d
hoosierEE <ashroyer@gmail.com>
2013-12-05 20:40:24 -0500

    image preview

--- Src/Components/LivePreview.elm
+++ Src/Components/LivePreview.elm
@@ -4,17 +4,4 @@ module Components.LivePreview where
 isbnUrl : String -> String
 isbnUrl isbn = "/php/checkISBN.php?isbn=" ++ isbn
 
-adUrl a = let scriptPath = "/php/storeAd.php"
-          in  scriptPath ++ "?user="        ++ a.user
-                         ++ "&Title="       ++ a.title
-                         ++ "&ISBN10="      ++ a.isbn10
-                         ++ "&ISBN13="      ++ a.isbn13
-                         ++ "&imageURL="    ++ a.imageUrl
-                         ++ "&ExpTime="     ++ a.expTime
-                         ++ "&Description=" ++ a.description
-                         ++ "&Condition="   ++ a.condition
-                         ++ "&Price="       ++ a.price
-                         ++ "&Adtype="      ++ a.adType
-                         ++ "&Lat="         ++ a.lat
-                         ++ "&Lon="         ++ a.lon
 
--- Src/MakeAd.elm
+++ Src/MakeAd.elm
@@ -4,17 +4,40 @@ import Components.UserInput   as Inputs
 import Components.LivePreview as Pre
 import Utils.Rest             as Rest
 
-display maker preview = flow right [ maker, asText preview ]
-
 isbnPreview = Rest.stringToRecord <~ (Rest.prettyPrint <~ (Rest.singleGet <| Pre.isbnUrl <~ (.isbn Inputs.presentRec)))
 
+storeAdRecord = { user        = "" 
+                , title       = ""
+                , isbn10      = ""
+                , isbn13      = ""
+                , imageUrl    = ""
+                , expTime     = ""
+                , description = ""
+                , condition   = ""
+                , price       = ""
+                , adType      = ""
+                , lat         = ""
+                , lon         = "" }
+                
+adUrl a = let scriptPath = "/php/storeAd.php"
+          in  scriptPath ++ "?user="        ++ a.user
+                         ++ "&Title="       ++ a.title
+                         ++ "&ISBN10="      ++ a.isbn10
+                         ++ "&ISBN13="      ++ a.isbn13
+                         ++ "&imageURL="    ++ a.imageUrl
+                         ++ "&ExpTime="     ++ a.expTime
+                         ++ "&Description=" ++ a.description
+                         ++ "&Condition="   ++ a.condition
+                         ++ "&Price="       ++ a.price
+                         ++ "&Adtype="      ++ a.adType
+                         ++ "&Lat="         ++ a.lat
+                         ++ "&Lon="         ++ a.lon
+               
 recordExtractor = 
-  let vinyl r = case r of
-    Nothing -> [""]
-    Just a  -> [a.valid, a.description, a.imageURL]
-  in vinyl <~ isbnPreview
+  let unrec r = case r of
+    Nothing -> ""
+    Just a  -> a.imageURL
+  in unrec <~ isbnPreview
 
+display maker preview = flow right [ maker, image 300 400 preview ]
 main = display <~ Inputs.adMaker ~ recordExtractor -- isbnPreview
-
--- debugging
--- isbnPreview = Pre.isbnUrl <~ (.isbn Inputs.presentRec)
