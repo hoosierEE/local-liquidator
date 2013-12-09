7022f5a4f02291535380f63a747df21ac2229bd7
hoosierEE <ashroyer@gmail.com>
2013-12-09 11:31:46 -0500

    maybe the AJAX works now...

--- Src/MakeAd.elm
+++ Src/MakeAd.elm
@@ -8,41 +8,47 @@ import Utils.Layout           as Layout
 isbnUrl : String -> String
 isbnUrl s = "/php/checkISBN.php?isbn=" ++ s
 
-theButton = (.butn Inputs.presentRec)
-user = Rest.helloUser
-userPlusButtons = [theButton, user]
+-- theButton = (.butn Inputs.presentRec)
+-- user = Rest.helloUser
+-- userPlusButtons = { adType = theButton
+-- , user = user }
 
 isbnPreview = Rest.stringToRecord <~ (Rest.prettyPrint <~ (Rest.singleGet <| isbnUrl <~ (.isbn Inputs.presentRec)))
 
 -- ajaxRecord = { 
 
-adUrl a = let scriptPath = "/php/storeAd.php"
-          in  scriptPath ++ "?user="        ++ a.user        
-                         ++ "&Title="       ++ a.title       
-                         ++ "&ISBN10="      ++ a.isbn10      
-                         ++ "&ISBN13="      ++ a.isbn13      
-                         ++ "&imageURL="    ++ a.imageUrl    
-                         ++ "&ExpTime="     ++ a.expTime     
-                         ++ "&Description=" ++ a.description 
-                         ++ "&Condition="   ++ a.condition   
-                         ++ "&Price="       ++ a.price       
-                         ++ "&Adtype="      ++ a.adType      
-                         ++ "&Lat="         ++ a.lat         
-                         ++ "&Lon="         ++ a.lon         
-
-recordExtractor fld = 
+adUrl =
+  let scriptPath user title isbn10 isbn13 imageUrl expTime description condition price adType lat lon =
+      "/php/storeAd.php?user=" ++ user        
+            ++ "&Title="       ++ title       
+            ++ "&ISBN10="      ++ isbn10      
+            ++ "&ISBN13="      ++ isbn13      
+            ++ "&imageURL="    ++ imageUrl    
+            ++ "&ExpTime="     ++ expTime     
+            ++ "&Description=" ++ description 
+            ++ "&Condition="   ++ condition   
+            ++ "&Price="       ++ price       
+            ++ "&Adtype="      ++ adType      
+            ++ "&Lat="         ++ lat         
+            ++ "&Lon="         ++ lon         
+  in Rest.singleGet <| sampleOn (.butn Inputs.presentRec) (scriptPath <~ Rest.helloUser ~ (extractor .title) ~ (extractor .isbn10)
+               ~ (extractor .isbn13) ~ (extractor .imageUrl) ~ (.expire Inputs.presentRec)
+               ~ (extractor .description) ~ (extractor .condition) ~ (.price Inputs.presentRec)
+               ~ (.butn Inputs.presentRec) ~ (extractor .lat) ~ (extractor .lon))
+              
+extractor fld = 
   let unrec r = case r of
     Nothing -> ""
     Just a  -> fld a
   in unrec <~ isbnPreview
 
-display w maker preview btn =
+display w maker preview =
   let previewTitle  = Layout.headerGen 300 28 darkOrange "Cover Preview"
       underLine     = spacer 300 2 |> color darkOrange
-      previewSide a = flow down [ previewTitle, underLine, a, asText btn ]
+      previewSide a = flow down [ previewTitle, underLine, a ]
       bumper        = spacer 10 10
       stuffing      = [ maker, bumper, previewSide <| image 300 400 preview ] 
       h             = maximum <| map heightOf stuffing
   in container w h middle <| flow right stuffing
 
-main = display <~ Window.width ~ Inputs.adMaker ~ (recordExtractor .imageUrl) ~ (combine userPlusButtons)
+main = display <~ Window.width ~ Inputs.adMaker ~ (extractor .imageUrl)
